ade561a95520bb44282bc96d9ec11823ac703472
Stefanie Fluin <sjtrimble@gmail.com>
2017-01-12 17:22:07 -0800

    add reset button, button styling

--- Cookie-Hackathon1/ViewController.swift
+++ Cookie-Hackathon1/ViewController.swift
@@ -2,37 +2,38 @@
 //  ViewController.swift
 //  Cookie-Hackathon1
 //
-//  Created by Stefanie Fluin on 1/12/17.
-//  Copyright © 2017 Stefanie Fluin. All rights reserved.
+//  Created by Team Cookie - Coding Dojo on 1/12/17.
+//  Copyright © 2017 Team Cookie - Codjing Dojo. All rights reserved.
+//  Developers: Stefanie Fluin, June Yoshii, Calvin Nguyen, Michael Imgrund
 //
-
-
-// velocity = distance/time
-// distance = velocity x time
-// Acceleration is the change in velocity over time
-// accel = vel / time
-//velocity = accell * time
-// interval = start to stop
-
-
 
 import UIKit
 import CoreMotion
 
 class ViewController: UIViewController {
     @IBOutlet weak var startStopButton: UIButton!
+    @IBOutlet weak var distanceResultLabel: UILabel!
     
     var startTime = Date()
     var time: Double = 0.0
     var acceleration: Double = 0.0
     var velocity: Double = 0.0
     var distance: Double = 0.0
+    let myColor = UIColor (red:53/255.0, green:169/255.0, blue:255/255.0, alpha:1.0)
+    
+    @IBOutlet weak var appNameLabel: UIButton!
     
     var motionManager: CMMotionManager?
     
+    @IBAction func resestHomePressed(_ sender: UIButton) {
+        distanceResultLabel.isHidden = true
+        resetVariables()
+    }
+    
     @IBAction func startButtonPressedDown(_ sender: UIButton) {
         sender.backgroundColor = UIColor.red
-        startStopButton.setTitle("Stop", for: .normal)
+        startStopButton.setTitle("Release to Stop", for: .normal)
+        distanceResultLabel.isHidden = true
         resetVariables()
         startTime = Date()
         print("startime: \(startTime)")
@@ -52,15 +53,23 @@ class ViewController: UIViewController {
     }
     
     @IBAction func stopButtonPressedUp(_ sender: UIButton) {
-        startStopButton.setTitle("Start", for: .normal)
+        startStopButton.setTitle("Press to Start", for: .normal)
+        sender.backgroundColor = myColor
+        distanceResultLabel.backgroundColor = myColor
+        distanceResultLabel.isHidden = false
         motionManager?.stopDeviceMotionUpdates()
         time = Double(Date().timeIntervalSince(startTime))
         print("time difference: \(time)")
         calculateDistance()
+        distanceResultLabel.text = String(Double(round(distance * 100)/100)) + " in"
+        distanceResultLabel.isHidden = false
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        appNameLabel.setTitle("Measure Ninja", for: .normal)
+        appNameLabel.backgroundColor = UIColor.black
+        distanceResultLabel.isHidden = true
     }
     
     func resetVariables() {
@@ -75,17 +84,17 @@ class ViewController: UIViewController {
     }
     
     func calculateDistance() {
-        print("accel \(acceleration)")
-        print("time \(time)")
+//        print("accel \(acceleration)")
+//        print("time \(time)")
         if acceleration < 0 {
             acceleration *= -1
         }
-        print("accel2 \(acceleration)")
+//        print("accel2 \(acceleration)")
         velocity = Double(acceleration) * time
-        print("velocity \(velocity)")
+//        print("velocity \(velocity)")
         let rawDistance = velocity * time * 9.8 * 39.37 // 1.10159 m/s2 43.369 in/s2
         distance = rawDistance.squareRoot()
-        print("dist \(distance) inches")
+//        print("dist \(distance) inches")
     }
     
     override func didReceiveMemoryWarning() {
